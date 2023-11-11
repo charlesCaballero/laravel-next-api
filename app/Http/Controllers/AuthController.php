3405a65f6dc054824e726b82278008c06599a9df
charlesCaballero <charles.g.caballero@gmail.com>
2023-11-11 16:06:48 +0800

    Using pure laravel sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /**
+     * Register a new user.
+     */
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // event(new Registered($user));
+
+        Auth::login($user);
+
+        $token = $user->createToken('token');
+
+        return response(['token' => $token->plainTextToken]);
+    }
+
+    /**
+     * Login user using credentials.
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // $request->session()->regenerate();
+            $token = $request->user()->createToken('token');
+
+            return response(['token' => $token->plainTextToken]);
+        }
+
+        return response(['error' => "The provided credentials do not match our records."]);
+    }
+
+    /**
+     * Logout current user.
+     */
+    public function logout(string $id)
+    {
+        //
+    }
+}
